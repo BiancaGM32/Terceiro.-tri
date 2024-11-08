627bdb344fd9223a84cf8765a08c5049fd60a8c7
Bianca Garcia Medina <111884359+BiancaGM32@users.noreply.github.com>
2024-11-07 21:08:43 -0300

    l

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,2 +1,9 @@
-informacoesGlobais.js
+const url="https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json"
+
+async function visualizarInformaçõesGlobais(){
+    constant resposta = await fetch (url)
+    let dados = await resposta.json;
+    console.log( dados );
+}
+visualizarInformaçõesGlobais();
 
